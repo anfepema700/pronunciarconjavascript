5b1a697df96807f1e3c7c5a07373fd5e50d6319b
anfepema700 <anfepema700@github.com>
2022-10-13 10:40:05 -0500

    update fileS

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 document.getElementById('hablar').addEventListener("click",()=>{decir(document.getElementById("texto").value);
 });
 
+
 function decir(texto){
+    if(texto != 0){
     speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+}else{
+    speechSynthesis.speak(new SpeechSynthesisUtterance("Escribe algo"));
+}
+
 }
